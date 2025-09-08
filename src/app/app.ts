20fe27ff38d69da18f85277a13c3cf0d92c724df
Ad-Gpta <aditibhoomika@gmail.com>
2025-09-08 07:04:28 +0000

    added basic login and score page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { ScorePage } from "./score-page/score-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ScorePage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('emoji_app');
+
+  //inject router
+  // router: Router = inject(Router);
+
+
+  startGame() {
+    // Logic to start the game
+    console.log('Game started!');
+    alert('Game started!');
+    // this.router.navigate(['/game']);
+  }
 }
